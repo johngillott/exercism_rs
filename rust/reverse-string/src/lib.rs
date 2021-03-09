2fe53030fc812e15c574b48cd3618a3eb0a54351
John Gillott <jsgtwenty@gmail.com>
2021-03-09 21:49:56 +0000

    Refactor reverse-string: unicode_segmentation crate to iterate over grapheme clusters

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,3 +1,13 @@
+use unicode_segmentation::UnicodeSegmentation;
+
+/*
+TIL Segmentation into Unicode grapheme clusters was supported by
+the standard library however was removed due to the size of the required
+Unicode tables. [source: https://stackoverflow.com/a/58770681]
+
+crate: https://docs.rs/unicode-segmentation/1.7.1/unicode_segmentation/struct.Graphemes.html
+*/
+
 pub fn reverse(input: &str) -> String {
-    input.chars().rev().collect()
+    input.graphemes(true).rev().collect::<std::string::String>()
 }
