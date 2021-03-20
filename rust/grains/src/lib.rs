0bf1dc7b4efa08b5bda7430f88d986e565799638
John Gillott <jsgtwenty@gmail.com>
2021-03-20 10:46:46 +0000

    rc grains: first draft

---
+++ rust/grains/src/lib.rs
@@ -0,0 +1,13 @@
+const MIN: u32 = 1;
+const MAX: u32 = 64;
+
+pub fn square(s: u32) -> u64 {
+    if s < MIN || s > MAX {
+        panic!("Square must be between 1 and 64")
+    }
+    (MIN..s).fold(1, |acc, _| acc * 2)
+}
+
+pub fn total() -> u64 {
+    (MIN..MAX + 1).fold(0, |acc, s| acc + square(s))
+}
