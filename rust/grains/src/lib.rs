1fbc01a97687c99392f824f56774bde4674754c7
John Gillott <jsgtwenty@gmail.com>
2021-03-20 11:24:49 +0000

    rustc grains refactor: eliminate square map.sum

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -3,11 +3,11 @@ const MAX: u32 = 64;
 
 pub fn square(s: u32) -> u64 {
     match s {
-        MIN..=MAX => (1..u64::from(s)).map(|x| 2 * x).sum(),
+        MIN..=MAX => 2u64.pow(s - 1),
         _ => panic!("Square must be between 1 and 64"),
     }
 }
 
 pub fn total() -> u64 {
-    (MIN..MAX + 1).map(square).sum()
+    (MIN..=MAX).map(square).sum()
 }
