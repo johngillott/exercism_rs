6b5b86e7cc2a1a0bedd51ff22b82d58387ff6d9a
John Gillott <jsgtwenty@gmail.com>
2021-04-10 13:50:20 +0100

    Rust clock: Display trait

---
+++ rust/clock/src/lib.rs
@@ -0,0 +1,36 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub struct Clock {
+    time: i32,
+}
+
+impl Clock {
+    pub fn new(hours: i32, minutes: i32) -> Self {
+        Clock {
+            time: hours * 60 + minutes,
+        }
+    }
+
+    pub fn add_minutes(&self, minutes: i32) -> Self {
+        unimplemented!("Add {} minutes to existing Clock time", minutes);
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_fmt(format_args!(
+            "{:02}:{:02}",
+            self.time / 60,
+            minutes = self.time % 60,
+        ))
+    }
+}
+
+impl PartialEq for Clock {
+    fn eq(&self, other: &Self) -> bool {
+        self == other
+    }
+}
+
+impl Eq for Clock {}
